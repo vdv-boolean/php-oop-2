24454aa45e402a45640ac9df5be276d00099d675
vincenzodivita <divitavincenzo@icloud.com>
2023-06-26 16:34:29 +0200

    Add classes

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+
+// Calling classes
+include_once __DIR__ . "/models/Product.php";
+include_once __DIR__ . "/models/Category.php";
+include_once __DIR__ . "/models/Food.php";
+include_once __DIR__ . "/models/Toy.php";
+include_once __DIR__ . "/models/Shelter.php";
+
