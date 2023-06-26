9f7cd5bf50f57e661afd421df7f0a1956b6e0330
vincenzodivita <divitavincenzo@icloud.com>
2023-06-26 15:24:53 +0200

    Add draft of construct fn

--- models/Product.php
+++ models/Product.php
@@ -3,4 +3,14 @@
 // Class declaration
 class Product {
 
-}
+    // Construct FN
+    public function __construct(
+        private int $id,
+        private string $name,
+        private string $description,
+        private int $price,
+        private Category $category,
+        private string $image
+    )
+    {}
+};
