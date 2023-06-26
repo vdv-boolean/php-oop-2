ac4bf107de2ad71dafa0f94998757db632d28824
vincenzodivita <divitavincenzo@icloud.com>
2023-06-26 22:40:15 +0200

    Add html tag

--- index.php
+++ index.php
@@ -9,5 +9,22 @@ include_once __DIR__ . "/models/Shelter.php";
 include __DIR__ . "/db.php";
 
 // Card Printing using FN
-foreach ($arrProducts as $product)
-    echo $product->printCard();
+// foreach ($arrProducts as $product)
+//     echo $product->printCard();
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Negozio per animali</title>
+</head>
+<body>
+    <?php
+    foreach ($arrProducts as $product)
+     echo $product->printCard();
+    ?>
+</body>
+</html>
