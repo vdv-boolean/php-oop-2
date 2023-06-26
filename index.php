f5d3731b0db9d36522f6b1db7e182dc4aa2785c3
vincenzodivita <divitavincenzo@icloud.com>
2023-06-26 17:16:05 +0200

    Include db.php, remove products and add for each loop

--- index.php
+++ index.php
@@ -6,46 +6,8 @@ include_once __DIR__ . "/models/Category.php";
 include_once __DIR__ . "/models/Food.php";
 include_once __DIR__ . "/models/Toy.php";
 include_once __DIR__ . "/models/Shelter.php";
-
-// Ex of food product
-$food = new Food(
-    1,
-    "cibo",
-    "descrizione del cibo",
-    999,
-    new Category(),
-    "",
-    "data",
-    1000
-);
-
-// Ex of toy product
-$toy = new Toy(
-    1,
-    "giocattolo",
-    "descrizione del giocattolo",
-    999,
-    new Category(),
-    "",
-    "materiale",
-    "colore"
-);
-
-// Ex of shelter product
-$shelter = new Shelter(
-    1,
-    "cuccia",
-    "descrizione della cuccia",
-    999,
-    new Category(),
-    "",
-    1000,
-    1000,
-    1000
-);
-
+include __DIR__ . "/db.php";
 
 // Card Printing using FN
-echo $food->printCard();
-echo $toy->printCard();
-echo $shelter->printCard();
+foreach ($arrProducts as $product)
+    echo $product->printCard();
