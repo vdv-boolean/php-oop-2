ac27b13f037558a118ee62cc41d075a037dc29aa
vincenzodivita <divitavincenzo@icloud.com>
2023-06-26 17:15:25 +0200

    add array of product

---
+++ db.php
@@ -0,0 +1,38 @@
+<?php
+
+$arrProducts = [
+new Food(
+    1,
+    "cibo",
+    "descrizione del cibo",
+    999,
+    new Category(),
+    "",
+    "data",
+    1000
+),
+
+new Toy(
+    1,
+    "giocattolo",
+    "descrizione del giocattolo",
+    999,
+    new Category(),
+    "",
+    "materiale",
+    "colore"
+),
+
+// Ex of shelter product
+new Shelter(
+    1,
+    "cuccia",
+    "descrizione della cuccia",
+    999,
+    new Category(),
+    "",
+    1000,
+    1000,
+    1000
+)
+];
